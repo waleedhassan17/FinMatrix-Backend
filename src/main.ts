75e3964b1b9eb1a515d1c8a47258a35fe6305f9e
Muhammad Waleed Hassan <waleedhassansfd@gmail.com>
2026-05-06 16:42:53 +0500

    fix: disable forbidNonWhitelisted to allow page/limit alongside query DTOs

--- src/main.ts
+++ src/main.ts
@@ -59,7 +59,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       transform: true,
-      forbidNonWhitelisted: true,
+      forbidNonWhitelisted: false,
       transformOptions: { enableImplicitConversion: true },
     }),
   );
